1be3e0c89b68166ec9ce37887fe59143c0038e1a
Vinicius VBS <vinyvilas@gmail.com>
2022-06-21 22:05:49 -0300

    User.entity correction

--- src/main/java/com/educandowebvinicius/course/entities/User.java
+++ src/main/java/com/educandowebvinicius/course/entities/User.java
@@ -6,8 +6,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "users")
 public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
 	

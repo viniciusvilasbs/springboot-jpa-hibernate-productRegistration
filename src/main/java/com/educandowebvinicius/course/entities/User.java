a4f5c9f55acc74fe65c943199d566900bbad8b30
Vinicius VBS <vinyvilas@gmail.com>
2022-07-08 10:47:18 -0300

    Order, Instant, ISO 8601 - Part1

--- src/main/java/com/educandowebvinicius/course/entities/User.java
+++ src/main/java/com/educandowebvinicius/course/entities/User.java
@@ -1,15 +1,18 @@
 package com.educandowebvinicius.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "users")
+@Table(name = "tb_user")
 public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
@@ -20,6 +23,9 @@ public class User implements Serializable{
 	private String email;
 	private String phone;
 	private String password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User(){
 	}
@@ -72,6 +78,10 @@ public class User implements Serializable{
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
+	public List<Order> getOrders() {
+		return orders;
+	}
 
 	@Override
 	public int hashCode() {
@@ -96,5 +106,5 @@ public class User implements Serializable{
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
-	}	
+	}
 }
